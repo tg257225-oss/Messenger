60e7b05a6265677308fde798b142df5d9408c720
Mapplect <tg257225@gmail.com>
2026-07-20 15:19:11 -0400

    Setting up client side

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+import threading
+host = '127.0.0.1'
+port = 2345
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if __name__ == '__main__':
+    main()
