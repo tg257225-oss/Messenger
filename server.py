d3cd6c7a635a9d5eeea47c7686a924714b085d02
Mapplect <tg257225@gmail.com>
2026-07-20 16:36:53 -0400

    Adds new users to the active_clients yet (hardwip)

--- server.py
+++ server.py
@@ -4,6 +4,23 @@ import threading
 host = '127.0.0.1'
 port = 2345
 listener_limit = 4
+active_clients = [] # ls of all active users
+
+# sends new msg to all clients on the server
+def send_msg_to_all(from_username, message):
+    pass
+
+
+def client_handler(client):
+
+    # server listens for client registering their username
+    while 1:
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+        else:
+            print("There is no username.")
+
 
 def main():
 
@@ -14,7 +31,7 @@ def main():
     try:
         # give server address in the form of host ip and port
         server.bind((host, port))
-        print("The server is now running.")
+        print(f"The server is now running on {host} {port}.")
     except:
         print(f"Unable to bind to the host {host} and port {port}.")
 
@@ -26,6 +43,8 @@ def main():
     while 1:
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}.")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 if __name__ == '__main__':
     main()
